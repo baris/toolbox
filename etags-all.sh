4a0fae0308fe9908cf16b9b1a19a28a04cdded7f
Baris Metin <bmetin@verivue.com>
2011-06-15 00:13:50 -0400

    tag .hh files

--- etags-all.sh
+++ etags-all.sh
@@ -4,4 +4,4 @@ if [[ -f TAGS ]]
 then
     rm TAGS
 fi
-find . -name '*.c' -o -name '*.h' -o -name '*.py' -o -name '*.cc' | xargs -n 1 etags --append
+find . -name '*.c' -o -name '*.h' -o -name '*.hh' -o -name '*.py' -o -name '*.cc' | xargs -n 1 etags --append
