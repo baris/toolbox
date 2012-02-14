cf56a40b54a5dd0103513d82f8b0d1967aa3a7d7
Baris Metin <bmetin@verivue.com>
2012-02-14 11:13:05 -0500

    fix a bunch of issues with up-all.sh

--- up-all.sh
+++ up-all.sh
@@ -1,24 +1,59 @@
 #!/bin/bash
 
-function run_verbose {
+if [[ $1 == "-v" ]]; then
+    VERBOSE=1
+    shift
+fi
+
+
+function _msg {
+    if [[ $VERBOSE -eq 1 ]]; then
+        echo -e $1
+    fi
+}
+
+function _run {
+    if [[ $2 -eq 1 ]]; then
+        $(eval $1)
+    else
+        eval $1 > /dev/null 2>&1
+    fi
+}
+
+function run {
     RUNCMD=$1
     RUNDIR=$2
-    echo "Running '$RUNCMD' on $(pwd)/$RUNDIR"
-    pushd $RUNDIR;
-    eval $RUNCMD;
-    popd
-    echo "done ($RUNDIR)."
-    echo -e "------\t------\t------\t------\t------\t------\t------"
+    _msg "Running '$RUNCMD' on $(pwd)/$RUNDIR"
+    _run 'pushd $RUNDIR'
+    _run 'eval $RUNCMD'
+    _run 'popd'
+}
+
+function git_branch () {
+    pushd $1 > /dev/null
+    gitbranch=$(git symbolic-ref HEAD 2> /dev/null)
+    gitbranch=${gitbranch#refs/heads/}
+    popd > /dev/null
+    echo $gitbranch
 }
 
 for i in `ls`
 do 
+    echo "#### Working on ${i} ####"
     if [ -f "${i}/.git/refs/remotes/git-svn" ]; then
-        run_verbose "git svn rebase" ${i}
+        run "git svn rebase" ${i}
     elif [ -d "${i}/.git/refs/remotes/origin" ]; then
-        run_verbose "git checkout master" ${i}
-        run_verbose "git pull" ${i}
+        CUR_BRANCH=$(git_branch ${i})
+        if [[ x$CUR_BRANCH != "xmaster" ]]; then
+            _msg "Changing current branch from ${CUR_BRANCH} to master"
+            run "git checkout master" ${i}
+        fi
+        run "git pull" ${i}
+        if [[ x$CUR_BRANCH != "xmaster" ]]; then
+            run "git checkout $CUR_BRANCH" ${i}
+            run "git pull" ${i}
+        fi
     elif [ -d "${i}/.svn/" ]; then
-        run_verbose "svn up" ${i}
+        run "svn up" ${i}
     fi
 done
